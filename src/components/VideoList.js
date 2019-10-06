84220294d751a646eb7b4b1717497e8edd6685e2
mehroozak <mehroozak@gmail.com>
2019-10-06 06:22:18 +0500

    task done = {button added, list added, env added, reshuffle integrated, code refactored, style updated}

---
+++ src/components/VideoList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import VideoItem from './VideoItem'
+
+const VideoList = ({videos, onVideoSelect}) => {
+  if (!videos) {
+    return <div>Loading...</div>;
+  }
+  const videoItems = videos.map((video) => {
+    return (
+      <VideoItem
+        onVideoSelect={ onVideoSelect }
+        key={video.etag}
+        video={video}
+      />
+  );
+  });
+
+  return (
+    <ul className="col-md-3 list-group">
+      { videoItems }
+    </ul>
+  );
+}
+
+export default VideoList;
